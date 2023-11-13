4947c135dbe1992fc9246516918a4f95a3cdbcaa
Arsenii <arsenii@gordspace.ru>
2023-11-13 21:57:55 +0300

    add animation.js

---
+++ css/animation.js
@@ -0,0 +1,35 @@
+const letters = split(animatable)
+setTimeout(() => letters.forEach(span => span.style.opacity = 1), 0)
+
+function split(element) {
+  element.style.wordBreak = 'break-word'
+
+  const letters = element.innerText.split('').map((letter, i) => {
+    const span = document.createElement('span')
+    span.innerHTML = letter !== ' ' ? letter : '&nbsp;'
+    span.style.opacity = 0
+    span.style.transitionDelay = 0.01 * i + 's'
+    return span
+  })
+
+  element.innerHTML = ''
+  element.append(...letters)
+  return letters
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const movingBlock = document.getElementById('moving-block');
+  
+    document.addEventListener('mousemove', function(e) {
+      const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+      const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  
+      movingBlock.style.transform = `translate(${xAxis}px, ${yAxis}px)`;
+    });
+  
+    // Добавляем обработчик события mouseover, чтобы запустить анимацию
+    movingBlock.addEventListener('mouseover', function() {
+      movingBlock.classList.add('animate');
+    });
+});  
+  
